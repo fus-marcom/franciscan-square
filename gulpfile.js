1cd646dfa43b2505d6b7d65f33aaa5e32a323a63
Jesse R Weigel <jesse@jesseweigel.com>
2016-10-03 16:10:18 -0400

    Stop uglify js

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('dist', function() {
          .pipe(usemin({
         assetsDir: 'app',
         css: [minifyCss(), 'concat'],
-        js: [babel(), uglify(), 'concat'],
+        js: [babel(), 'concat'],
         html: [ htmlmin({
           collapseBooleanAttributes: true,
           collapseWhitespace: true,
